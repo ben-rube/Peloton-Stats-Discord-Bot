d990e54eae42a36b8f8cd647fe43a63730cbe2d0
Ben Rubenstein <5686050+ben-rube@users.noreply.github.com>
2022-05-29 01:06:19 -0400

    Cleanup http server (#1)
    * added http server framework
    
    * added basic http server for adding pton account
    
    * cleanup

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+
+function InitServer(port) {
+	app.listen(port, () => {
+		console.log(`Example app listening on port ${port}`);
+	});
+}
+
+module.exports = {
+	InitServer,
+};
