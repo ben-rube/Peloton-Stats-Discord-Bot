3b0671d8c0211d5bef1cd03e0313d7e601268856
Ben Rubenstein <ben@rube.pw>
2022-05-29 01:05:30 -0400

    cleanup

--- server/server.js
+++ server/server.js
@@ -6,8 +6,12 @@ app.get('/', (req, res) => {
 });
 
 
-export function InitServer(port) {
-	app.express.listen(port, () => {
+function InitServer(port) {
+	app.listen(port, () => {
 		console.log(`Example app listening on port ${port}`);
 	});
 }
+
+module.exports = {
+	InitServer,
+};
